99fca86a44d8e174c2ff45bb5142ec3b7383b827
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-04-07 22:39:01 +0200

    Update geopath

--- R/geopath.r
+++ R/geopath.r
@@ -4,50 +4,92 @@
 #' 
 #' @export
 geopath <- function(
-    fill.color = "pink", 
-    hover.color = "lightblue",
-    border.color = "black",
-    url = "data/us-states.json",
-    projection = "albersUsa",
+    data = NULL,  # variable data
+    id = names(data),  # id vector (e.g. wdi codes for matching wdi.geo.json)
+    fill.color = "lightblue", 
+    hover.color = "blue",
+    border.color = "white",
+    border.width = 0.25,
+    url = "json/wdi.geo.json",
+    projection = "orthographic",
     scale = 500,
     translate = c(a$opt$width / 2,  a$opt$height / 2),  # as default map position is centered
     ...
     ) {
+    
+    # OBS:
+    ## Currently requires that the data/id include all observations available in geoJSON
+    
+    # Prepare empty labels if set to null
+    if (is.null(id)) {
+        id <- rep("", length(data))
+    }
+    
+    # Prepare data
+    x <- to_array(
+        data.frame(
+            key = id, 
+            val = data,
+            stringsAsFactors = FALSE
+        )
+    )
 
     # Create Vega object
     a <- rVega:::Vega$new(...)
 
     a$data(
-        name = "world",
+        name = "variable",
+        values = x
+    )
+
+    a$data(
+        name = "map",
         url = url,
         format = list(
             type = "json",
             property = "features"
+        ),
+        transform = list(
+            list(
+                type = "geopath",
+                value = "data",
+                projection = projection,
+                scale = scale
+            ),
+            list(
+                type = "zip",
+                key = "data.id",
+                with = "variable",
+                withKey = "data.key",
+                as = "value"
+            )
         )
     )
-
+    
+    a$scales(
+        name = "color",
+        domain = list(
+            data = "variable", 
+            field = "data.val"
+        ),
+        range = c("yellow", "red")
+    )
+    
     a$marks(
         type = "path",
         from = list(
-            data = "world",
-            transform = list(
-                list(
-                    type = "geopath",
-                    value = "data",
-                    projection = projection,
-                    scale = scale,
-                    translate = translate
-                )
-            )
+            data = "map"
         ),
         properties = list(
             enter = list(
                 stroke = list(value = border.color),
+                strokeWidth = list(value = border.width),
                 path = list(field = "path")
             ),
             update = list(
                 fill = list(
-                    value = fill.color
+                    scale = "color",
+                    field = "value.data.val"
                 )
             ),
             hover = list(
